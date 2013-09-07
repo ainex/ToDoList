e8b0f826c251e9e8f8b7bc67625a8a42e6adaf26
ainex <ainex.niene@gmail.com>
2013-09-07 12:28:55 +0700

    project structure added

---
+++ src/main/java/com.ulyanova.todo/service/Controller.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ainex
+ * Date: 07.09.13
+ * Time: 11:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controller {
+
+    public static void main (String[] args){
+
+        Connection connection = H2DatabaseConnectionUtil.getDbConnection();
+        //connection
+    }
+}
---
+++ src/main/java/com.ulyanova.todo/service/H2DatabaseConnectionUtil.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ainex
+ * Date: 07.09.13
+ * Time: 11:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class H2DatabaseConnectionUtil {
+
+    private static final String DRIVER_NAME = "org.h2.Driver";
+    private static Connection dbConnection;
+
+    static {
+
+        try {
+            Class.forName(DRIVER_NAME);
+            dbConnection = DriverManager.getConnection("jdbc:h2:mem:toDoDatabase", "admin", "password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getDbConnection(){
+        return dbConnection;
+    }
+
+
+}
